6f06a8e1ec6b19f1e029401fa4b25c4fc76f43dd
Your Name <you@example.com>
2022-08-23 13:33:07 +0000

    アプリケーションの作成

---
+++ app/models/book.rb
@@ -0,0 +1,26 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  
+  def favorited?(user)
+   favorites.where(user_id: user.id).exists?
+  end
+  
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @book = Book.where("title LIKE?", "#{word}")
+    elsif search == "foword_match"
+      @book = Book.where("title LIKE?", "#{word}%")
+    elsif search == "backword_match"
+      @book = Book.where("title LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @Book = Book.where("title LIKE?", "%#{word}%")
+    else
+      @book = Book.all
+    end
+  end
+end
